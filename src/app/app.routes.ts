f48ce7c82fb4682f32f9439f8d01bfa2bd56b9f3
Muhammad Faiz Shahid <mfaizshahid.me@gmail.com>
2024-12-19 22:10:21 +0500

    chore: init routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {PublicComponent} from './public/public.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PublicComponent },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
